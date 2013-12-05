13e097a33340ec8d542e01880cd57ea9b9d456a3
Zero Ability <dml337ira@gmail.com>
2013-12-05 17:42:23 +0000

    initial commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'fa17fe42d24936dc7f096e4056f4f549c5133a07219d083cad5ac60f4c0ac985cc77536da5074ad153747f7330a796dc1c7b57bfa8d8768dcad1ab49f2c63253'
